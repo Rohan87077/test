625d73b52f9e61fb1bbee92ccea8ad77aba1604e
Rohan <rohan@gmail.com>
2026-01-04 20:52:30 +0530

    second commit

--- Revision.py
+++ Revision.py
@@ -42,17 +42,27 @@
 
 
 
-# m1 = int(input("Enter marks of student 1: "))
-# m2 = int(input("Enter marks of student 2: "))
-# m3 = int(input("Enter marks of student 3: "))
-# m4 = int(input("Enter marks of student 4: "))
-# m5 = int(input("Enter marks of student 5: "))
-# m6 = int(input("Enter marks of student 6: "))
+Name1 = "a"
+m1 = int(input("Enter marks of student 1: "))
+Name2 = "b"
+m2 = int(input("Enter marks of student 2: "))
+Name3 = "c"
+m3 = int(input("Enter marks of student 3: "))
+Name4 = "d"
+m4 = int(input("Enter marks of student 4: "))
+Name5 = "d"
+m5 = int(input("Enter marks of student 5: "))
+Name6 = "e"
+m6 = int(input("Enter marks of student 6: "))
+Name7 = "f"
+m7 = int(input("Enter marks of student 7: "))
 
-# marks = [m1,m2,m3,m4,m5,m6]
-# marks.sort()
-# print(marks)
+name = [Name1,Name2,Name3,Name4,Name5,Name6,Name7]
+print(name)
+marks = [m1,m2,m3,m4,m5,m6,m7]
+marks.sort()
+print(marks)
 
-a = (55.5,)
-t = type(a)
-print(t)
+# a = (55.5,)
+# t = type(a)
+# print(t)
