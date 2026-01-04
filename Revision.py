051063e79106781dd82284ec260ac3a490b00686
Rohan <rohan@gmail.com>
2026-01-04 20:46:40 +0530

    First commit

---
+++ Revision.py
@@ -0,0 +1,58 @@
+# a = "Rohan  is gud"
+# f = a.find("  ")
+# print(f)
+
+# b = "Rohanis gud"
+# f2 = b.find(" ")
+# print(f2)
+
+# letter = "Dear Harry,\n \tthis python course is nice.\n \t\tThanks!"
+# print(letter)
+
+# Write a program to store seven fruits in a list entered by the user.
+
+# a = input("Enter fruit 1")
+# b = input("Enter fruit 2")
+# c = input("Enter fruit 3")
+# d = input("Enter fruit 4")
+# e = input("Enter fruit 5")
+# f = input("Enter fruit 6")
+# g = input("Enter fruit 7")
+
+# fruits = [a,b,c,d,e,f,g]
+
+# print(fruits)
+
+
+# Write a program to accept marks of 6 students and display them in a sorted
+# manner
+
+# a1 = input("Enter name")
+# a = int(input("Enter marks of student 1: "))
+# b1 = input("Enter name")
+# b = int(input("Enter marks of student 2: "))
+# c1 = input("Enter name")
+# c = int(input("Enter marks of student 3: "))
+# d1 = input("Enter name")
+# d = int(input("Enter marks of student 4: "))
+# e1 = input("Enter name")
+# e = int(input("Enter marks of student 5: "))
+# f1 = input("Enter name")
+# f = int(input("Enter marks of student 6: "))
+
+
+
+# m1 = int(input("Enter marks of student 1: "))
+# m2 = int(input("Enter marks of student 2: "))
+# m3 = int(input("Enter marks of student 3: "))
+# m4 = int(input("Enter marks of student 4: "))
+# m5 = int(input("Enter marks of student 5: "))
+# m6 = int(input("Enter marks of student 6: "))
+
+# marks = [m1,m2,m3,m4,m5,m6]
+# marks.sort()
+# print(marks)
+
+a = (55.5,)
+t = type(a)
+print(t)
