56d928c6fd1444e881a27c123f85c61cbc15c927
Rohan <rohan@gmail.com>
2026-01-06 19:01:49 +0530

    Revision.py

--- Revision.py
+++ Revision.py
@@ -56,10 +56,16 @@ Name6 = "e"
 m6 = int(input("Enter marks of student 6: "))
 Name7 = "f"
 m7 = int(input("Enter marks of student 7: "))
+Name8 = "f"
+m8 = int(input("Enter marks of student 8: "))
+Name9 = "f"
+m9 = int(input("Enter marks of student 9: "))
+Name10 = "f"
+m10 = int(input("Enter marks of student 10: "))
 
-name = [Name1,Name2,Name3,Name4,Name5,Name6,Name7]
+name = [Name1,Name2,Name3,Name4,Name5,Name6,Name7,Name8,Name9,Name10]
 print(name)
-marks = [m1,m2,m3,m4,m5,m6,m7]
+marks = [m1,m2,m3,m4,m5,m6,m7,m8,m9,m10]
 marks.sort()
 print(marks)
 
