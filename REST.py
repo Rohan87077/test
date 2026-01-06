7f52c1264ef66b6b2cc934dc652d27a527b3a554
Rohan <rohan@gmail.com>
2026-01-06 19:46:30 +0530

    First ONE

---
+++ REST.py
@@ -0,0 +1,26 @@
+Name1 = "a"
+m1 = int(input("Enter marks of student 1: "))
+Name2 = "b"
+m2 = int(input("Enter marks of student 2: "))
+Name3 = "c"
+m3 = int(input("Enter marks of student 3: "))
+Name4 = "d"
+m4 = int(input("Enter marks of student 4: "))
+Name5 = "d"
+m5 = int(input("Enter marks of student 5: "))
+Name6 = "e"
+m6 = int(input("Enter marks of student 6: "))
+Name7 = "f"
+m7 = int(input("Enter marks of student 7: "))
+Name8 = "f"
+m8 = int(input("Enter marks of student 8: "))
+Name9 = "f"
+m9 = int(input("Enter marks of student 9: "))
+Name10 = "f"
+m10 = int(input("Enter marks of student 10: "))
+
+name = [Name1,Name2,Name3,Name4,Name5,Name6,Name7,Name8,Name9,Name10]
+print(name)
+marks = [m1,m2,m3,m4,m5,m6,m7,m8,m9,m10]
+marks.sort()
+print(marks)
