82eb5a293832d4b390328dae112372a82d4f6cf4
Rohan <rohan@gmail.com>
2026-01-06 20:16:18 +0530

    LAST COMMIT

--- REST.py
+++ REST.py
@@ -10,8 +10,10 @@ Name5 = "d"
 m5 = int(input("Enter marks of student 5: "))
 Name6 = "e"
 m6 = int(input("Enter marks of student 6: "))
-name = [Name1,Name2,Name3,Name4,Name5,Name6]
+Name7 = "f"
+m7 = int(input("Enter marks of student 7: "))
+name = [Name1,Name2,Name3,Name4,Name5,Name6,Name7]
 print(name)
-marks = [m1,m2,m3,m4,m5,m6]
+marks = [m1,m2,m3,m4,m5,m6,m7]
 marks.sort()
 print(marks)
